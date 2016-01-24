33f25f00e6cdae0d7097678dfd2de4cffc72c4eb
Trammell Hudson <hudson@osresearch.net>
2016-01-24 16:07:57 -0500

    servo command works, but steps very slow

--- servostep.ino
+++ servostep.ino
@@ -4,7 +4,9 @@
  */
 #include "QuadDecode.h"
 
-static int16_t old_count;
+
+#define K_eps 10	// how close is "good enough"
+
 
 #define STEP_PIN 10
 #define DIR_PIN 13
@@ -16,31 +18,110 @@ void setup()
 	pinMode(DIR_PIN, OUTPUT);
 }
 
-void loop()
+static int16_t target;
+
+void
+servo_cmd(
+	int cmd
+)
 {
-	int16_t count = QuadDecode.getCounter1();
-	int16_t delta = count - old_count;
+	if (cmd < -65536 || cmd >= +65536)
+	{
+		Serial.println("range");
+		return;
+	}
 
-	Serial.printf( "%+6d %+6d\r\n", count, delta);
-	old_count = count;
+	target = cmd;
+}
 
-	if (delta == 0)
+void
+read_command()
+{
+	static int flush = 0;
+	static int new_cmd = 0;
+	static int sign = 1;
+
+	if (!Serial.available())
 		return;
 
-	int sign = 1;
-	if (delta < 0)
+	const int c = Serial.read();
+	if (c == -1)
+		return;
+
+	if (c == '-')
 	{
-		sign = 0;
-		delta = -delta;
+		sign = -1;
+		return;
 	}
-	
-	digitalWriteFast(DIR_PIN, sign);
-	delayMicroseconds(100);
-	//for(int i = 0 ; i < delta ; i++)
+	if (c == '+')
 	{
-		digitalWriteFast(STEP_PIN, 1);
-		delayMicroseconds(10000);
-		digitalWriteFast(STEP_PIN, 0);
-		delayMicroseconds(10000);
+		sign = +1;
+		return;
+	}
+
+	if ('0' <= c && c <= '9')
+	{
+		new_cmd = 10 * new_cmd + c - '0';
+		return;
+	}
+		
+	if (c == '\n')
+	{
+		if (!flush)
+			servo_cmd(sign * new_cmd);
+		flush = 0;
+		new_cmd = 0;
+		sign = 1;
+		return;
+	}
+
+	if (c == '\r')
+		return;
+
+	Serial.print('?');
+	new_cmd = 0;
+	flush = 1;
+	sign = 1;
+	return;
+}
+
+void loop()
+{
+	// check for a command
+	read_command();
+
+	// run the control loop at 1 KHz
+	static unsigned last_now;
+	const unsigned now = millis();
+	if (now == last_now)
+		return;
+	last_now = now;
+
+	static unsigned update_rate;
+	static int16_t old_count;
+	int16_t count = QuadDecode.getCounter1();
+	int16_t delta = count - old_count;
+	old_count = count;
+
+	if ((update_rate++ % 16) == 0)
+		Serial.printf( "%+6d %+6d %+6d\r\n", count, delta, target);
+
+	int error = target - count;
+
+	if (error < 0)
+	{
+		digitalWriteFast(DIR_PIN, 0);
+		error = -error;
+	} else {
+		digitalWriteFast(DIR_PIN, 1);
+	}
+
+	if (error < K_eps)
+	{
+		// do nothing
+	} else
+	{
+		// step
+		digitalWriteFast(STEP_PIN, update_rate & 1);
 	}
 }
